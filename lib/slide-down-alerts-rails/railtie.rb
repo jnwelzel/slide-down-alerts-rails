3f5e41c06642062e857e1e0e8d2dbcbb5425e3f1
Jon Welzel <jnwelzel@gmail.com>
2013-03-26 00:03:32 -0300

    all seems fine now

---
+++ lib/slide-down-alerts-rails/railtie.rb
@@ -0,0 +1,15 @@
+require 'slide-down-alerts-rails/view_helpers'
+
+module Slide
+  module Down
+    module Alerts
+      module Rails
+      	class Railtie < ::Rails::Railtie
+			    initializer "slide-down-alerts-rails.view_helpers" do
+			      ActionView::Base.send :include, ViewHelpers
+			    end
+			  end
+      end
+    end
+  end
+end
